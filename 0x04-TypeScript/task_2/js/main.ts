9cee4169d58e4e05f0ad225fda06fb6a40cbd92d
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-11-08 15:54:02 +0300

    asdsa

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -38,21 +38,37 @@ class Teacher implements TeacherInterface {
   }
 }
 
+// createEmployee function
 function createEmployee(salary: number | string): Director | Teacher {
-  const numericSalary = typeof salary === 'number' 
-    ? salary 
-    : parseInt(salary.replace('$', ''));
+  if (typeof salary === 'string') {
+    salary = parseInt(salary.replace('$', ''));
+  }
 
-  return numericSalary < 500 ? new Teacher() : new Director();
+  if (salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
 }
 
-// Tests
+// Type literal
+type Subjects = 'Math' | 'History';
+
+// teachClass function
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+  return '';
+}
+
+// Example tests
 console.log(createEmployee(200));    // Teacher
 console.log(createEmployee(1000));   // Director
 console.log(createEmployee('$500')); // Director
 
-
-// Test
-console.log(createEmployee(200));    // Teacher
-console.log(createEmployee(1000));   // Director
-console.log(createEmployee('$500')); // Director
+console.log(teachClass('Math'));     // Teaching Math
+console.log(teachClass('History'));  // Teaching History
