68c28d8227484b37c33d22758001eed09d7fd6de
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-11-08 15:52:56 +0300

    fxed

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -38,37 +38,21 @@ class Teacher implements TeacherInterface {
   }
 }
 
-// createEmployee function
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'string') {
-    salary = parseInt(salary.replace('$', ''));
-  }
+  const numericSalary = typeof salary === 'number' 
+    ? salary 
+    : parseInt(salary.replace('$', ''));
 
-  if (salary < 500) {
-    return new Teacher();
-  } else {
-    return new Director();
-  }
+  return numericSalary < 500 ? new Teacher() : new Director();
 }
 
-// Type literal
-type Subjects = 'Math' | 'History';
-
-// teachClass function
-function teachClass(todayClass: Subjects): string {
-  if (todayClass === 'Math') {
-    return 'Teaching Math';
-  }
-  if (todayClass === 'History') {
-    return 'Teaching History';
-  }
-  return '';
-}
-
-// Example tests
+// Tests
 console.log(createEmployee(200));    // Teacher
 console.log(createEmployee(1000));   // Director
 console.log(createEmployee('$500')); // Director
 
-console.log(teachClass('Math'));     // Teaching Math
-console.log(teachClass('History'));  // Teaching History
+
+// Test
+console.log(createEmployee(200));    // Teacher
+console.log(createEmployee(1000));   // Director
+console.log(createEmployee('$500')); // Director
