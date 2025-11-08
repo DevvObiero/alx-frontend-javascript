49a3280602b5fd2afe7bc640f49007a0e285157d
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-11-08 14:32:01 +0300

    errors

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -51,4 +51,4 @@ function createEmployee(salary: number | string): Director | Teacher {
 // Test
 console.log(createEmployee(200));    // Teacher
 console.log(createEmployee(1000));   // Director
-console.log(createEmployee('$500')); // Director
+console.log(createEmployee('$500')); 
